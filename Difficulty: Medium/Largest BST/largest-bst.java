369784de755e97fb051a2337808ac0352e8ceb6e
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2024-11-05 19:48:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.java
@@ -0,0 +1,145 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+import java.math.*;
+
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+
+class GFG
+{
+    static Node buildTree(String str)
+    {
+        // Corner Case
+        if(str.length() == 0 || str.equals('N'))
+            return null;
+        String[] s = str.split(" ");
+        
+        Node root = new Node(Integer.parseInt(s[0]));
+        Queue <Node> q = new LinkedList<Node>();
+        q.add(root);
+        
+        // Starting from the second element
+        int i = 1;
+        while(!q.isEmpty() && i < s.length)
+        {
+              // Get and remove the front of the queue
+              Node currNode = q.remove();
+        
+              // Get the curr node's value from the string
+              String currVal = s[i];
+        
+              // If the left child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the left child for the curr node
+                  currNode.left = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.left);
+              }
+        
+              // For the right child
+              i++;
+              if(i >= s.length)
+                  break;
+              currVal = s[i];
+        
+              // If the right child is not null
+              if(!currVal.equals("N")) 
+              {
+        
+                  // Create the right child for the curr node
+                  currNode.right = new Node(Integer.parseInt(currVal));
+        
+                  // Push it to the queue
+                  q.add(currNode.right);
+              }
+              
+              i++;
+        }
+    
+        return root;
+    }
+    
+    public static void main(String args[]) throws IOException {
+    
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while(t>0)
+        {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            
+            Solution T = new Solution();
+            System.out.println(T.largestBst(root));
+            
+            t--;
+        
+System.out.println("~");
+}
+    }
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+// class Node  
+// { 
+//     int data; 
+//     Node left, right; 
+   
+//     public Node(int d)  
+//     { 
+//         data = d; 
+//         left = right = null; 
+//     } 
+// }
+
+
+class Solution{
+    
+    // Return the size of the largest sub-tree which is also a BST
+    static class NodeValue{
+        int minNode,maxNode,maxSize;
+        NodeValue(int minNode,int maxNode, int maxSize){
+            this.minNode=minNode;
+            this.maxNode=maxNode;
+            this.maxSize=maxSize;
+        }
+    }
+    static int largestBst(Node root){
+        // Write your code here
+        return findLargestBST(root).maxSize;
+        
+    }
+    static NodeValue findLargestBST(Node root){
+        if(root==null) return new NodeValue(Integer.MAX_VALUE,Integer.MIN_VALUE,0);
+        
+        NodeValue left=findLargestBST(root.left);
+        NodeValue right=findLargestBST(root.right);
+        
+        if(left.maxNode<root.data && root.data<right.minNode) 
+            return new NodeValue(Math.min(root.data,left.minNode),Math.max(root.data,right.maxNode),left.maxSize+right.maxSize+1);
+        else 
+            return new NodeValue(Integer.MIN_VALUE,Integer.MAX_VALUE,Math.max(left.maxSize,right.maxSize));
+    }
+    
+}
