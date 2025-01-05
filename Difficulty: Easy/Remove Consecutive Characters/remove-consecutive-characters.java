54e92567848aef95b31789691d1ea7ed6d948fb1
Arijit2003 <91794464+Arijit2003@users.noreply.github.com>
2025-01-05 22:36:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Consecutive Characters/remove-consecutive-characters.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+
+            String S = read.readLine().trim();
+
+            Solution ob = new Solution();
+            String ans = ob.removeConsecutiveCharacter(S);
+
+            System.out.println(ans);
+
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public String removeConsecutiveCharacter(String s) {
+        // code here
+        StringBuilder sb=new StringBuilder("");
+        sb.append(s.charAt(0));
+        for(int i=1;i<s.length();i++){
+            if(sb.charAt(sb.length()-1)!=s.charAt(i))sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+}
